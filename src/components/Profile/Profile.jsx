bb1aa6074c5038ec953296307b9b57f234927f50
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-20 11:39:04 +0200

    Initial commit

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import css from "./Profile.module.css";
+
+export const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={image} alt={name} width="300" className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
