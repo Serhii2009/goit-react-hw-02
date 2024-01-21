d0cadd358b404103ff4e56feac5fba2d73d04ba1
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-01-21 21:40:03 +0200

    Rewrite some styles

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,15 @@
 export const Options = ({ onFeedback }) => {
   return (
     <div>
-      <button onClick={() => onFeedback("good")}>Good</button>
-      <button onClick={() => onFeedback("neutral")}>Neutral</button>
-      <button onClick={() => onFeedback("bad")}>Bad</button>
+      <button className="list-buttons" onClick={() => onFeedback("good")}>
+        Good
+      </button>
+      <button className="list-buttons" onClick={() => onFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className="list-buttons" onClick={() => onFeedback("bad")}>
+        Bad
+      </button>
     </div>
   );
 };
